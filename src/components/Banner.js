226216570221876d33282f890418b197391ccbad
brunio97 <brusalinasv1997@gmail.com>
2024-04-12 16:11:09 -0400

    test

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,6 +15,19 @@ const Banner = () => {
   const [selectedValueCarPrice, setSelectedValueCarPrice] = useState('');
 
 
+  const [city,setCity]=useState("")
+  const [zip,setZip]=useState("")
+  const [type,setType]=useState("")
+  const [model,setModel]=useState("")
+  const [pickUpDate,setPickupDate]=useState("")
+  const [pickUpTime,setPickupTime]=useState("")
+  const [returnDate,setReturnDate]=useState("")
+  const [returnTime,setReturnTime]=useState("")
+  const [price,setPrice] = useState("")
+  const [age,setAge] = useState("")
+  const [phone,setPhone] = useState("")
+  const [email,setEmail] = useState("")
+
   const formInitialDetails = {
     city: '',
     zip: '',
@@ -103,26 +116,24 @@ const Banner = () => {
     <NavBar/>
     <section className="banner" style={{marginTop:banM}}>
       <Container >
-          <form onSubmit={handleSubmit} method="POST" className="centered-form">
+          <form name="rentalcars" method="POST" className="centered-form">
+          <input type="hidden" name="form-name" value="rentalcars"/>
             <h1>Reserve a Vehicle</h1>
             <Row className="banner-res">
               <Col>
                 <label>City</label><br/>
-                  <input type="text" value={formDetails.city} placeholder="City" onChange={(e) => onFormUpdate('city', e.target.value)} required/>  
+                  <input type="text" placeholder="City" value={city} name="city" id="city"  onChange={(e)=>setCity(e.target.value)} required/> 
               </Col>
               <Col>
                 <label>Zip</label><br/>
-                  <input type="text" value={formDetails.zip} placeholder="zip" onChange={(e) => onFormUpdate('zip', e.target.value)} required/>
+                  <input type="text" placeholder="zip" value={zip} name="zip" id="zip"  onChange={(e)=>setZip(e.target.value)} required/>
               </Col>
             </Row>
 
             <Row className="banner-res">
               <Col >
                 <label>Type of Car</label> <br/>
-                  <select id="car" name="car" onChange={(e) => {
-                    onFormUpdate('type', e.target.value);
-                    handleSelectChange(e);
-                  }} required>
+                  <select id="type" name="type" value={type} onChange={(e)=>setType(e.target.value)} required>
                     <option value="" >Select your type of car:</option>
                     <option value="compact">Compact Cars</option>
                     <option value="SUV">SUVs</option>
@@ -136,10 +147,7 @@ const Banner = () => {
                 {carSelected[0]?(
                 <Col >
                   <label>{titleCar}</label><br/>
-                  <select value={selectedValueCar} id="carModel" name="carModel" onChange={(e) => {
-                    onFormUpdate('model', e.target.value);
-                    handleSelectChangeCar(e);
-                  }} required >
+                  <select id="model" name="model" value={model} onChange={(e)=>setModel(e.target.value)} required >
                     <option value="" >Select your type of car:</option>
                     {typeCar.map((car,index)=>(
                         <option key={index} value={car.title}>{car.text}</option>
@@ -164,25 +172,22 @@ const Banner = () => {
             <Row className="banner-res">  
               <Col>
                 <label>Pick-Up</label><br/>
-                  <input type="date" id="date" name="date" value={formDetails.pickUpDate} onChange={(e) => onFormUpdate('pickUpDate', e.target.value)} required/>
-                  <input type="time" id="time" name="time" value={formDetails.pickUpTime} onChange={(e) => onFormUpdate('pickUpTime', e.target.value)} required/>
+                  <input type="date" id="pickUpDate" name="pickUpDate" value={pickUpDate} onChange={(e)=>setPickupDate(e.target.value)} required/>
+                  <input type="time" id="pickUpTime" name="pickUpTime" value={pickUpTime} onChange={(e)=>setPickupTime(e.target.value)} required/>
               </Col>
               <Col>
               <label>Return</label><br/>
-                  <input type="date" id="date1" name="date1" value={formDetails.returnDate} onChange={(e) => onFormUpdate('returnDate', e.target.value)} required/>
-                  <input type="time" id="time1" name="time1" value={formDetails.returnTime} onChange={(e) => onFormUpdate('returnTime', e.target.value)} required/>
+                  <input type="date" id="returnDate" name="returnDate" value={returnDate} onChange={(e)=>setReturnDate(e.target.value)} required/>
+                  <input type="time" id="returnTime" name="returnTime" value={returnTime} onChange={(e)=>setReturnTime(e.target.value)} required/>
               </Col>
             </Row>
 
             <Row className="banner-res">
-<Col>
+              <Col>
               {carSelected[1]?(
                 <div className="priceCar">
                 <label>Select Price</label><br/>
-                  <select value={selectedValueCarPrice} className="priceCar"  id="carPrice" name="carPrice" onChange={(e) => {
-                    onFormUpdate('price', e.target.value);
-                    handleSelectChangeCarPrice(e);
-                  }} required>
+                  <select value={price} className="priceCar"  id="price" name="price" onChange={(e)=>setPrice(e.target.value)} required>
                     <option value="">Select your type of car:</option>
                     {typeCarImg.map((car,index)=>(
                         <option key={index} value={car.price}>{car.price}</option>
@@ -196,9 +201,7 @@ const Banner = () => {
                   </Col>
                   <Col sm={carSelected[1]?null:12}>
               <label>Render Age</label><br/>
-                <select id="age" name="age" onChange={(e) => {
-                    onFormUpdate('age', e.target.value);
-                  }} required>
+                <select id="age" name="age" value={age} onChange={(e)=>setAge(e.target.value)} required>
                   <option value="" >Select your age:</option>
                   <option value='18'>18</option>
                   <option value='19'>19</option>
@@ -214,11 +217,11 @@ const Banner = () => {
             <Row className="banner-res">
               <Col>
                 <label>Phone Number</label><br/>
-                <input type="tel" value={formDetails.number} pattern="[+]?[0-9\s-]*" placeholder="+1 XXX XXX XXXX" onChange={(e) => onFormUpdate('number', e.target.value)} required/>   
+                <input type="tel" name="phone" id="phone" value={phone} pattern="[+]?[0-9\s-]*" placeholder="+1 XXX XXX XXXX" onChange={(e)=>setPhone(e.target.value)} required/>   
               </Col>
               <Col >
                 <label>Email address</label><br/>
-                <input type="email" value={formDetails.email} placeholder="name@example.com" onChange={(e) => onFormUpdate('email', e.target.value)} required/>
+                <input type="email" value={email} placeholder="name@example.com" name="email" id="email" onChange={(e)=>setEmail(e.target.value)} required/>
               </Col>
             </Row>
             <Row className="banner-res">
@@ -227,12 +230,12 @@ const Banner = () => {
                   <span>{buttonText}</span>
                   </Button>
                 </Col>    
-                {
+                {/* {
                     status.message &&
                     <Col>
                       <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
                     </Col>
-                  }
+                  } */}
             </Row>
           </form>
 
